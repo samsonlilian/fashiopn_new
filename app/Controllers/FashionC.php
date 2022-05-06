2b360c600b04ef252dd521008eb8f4bf9617a0e2
Lilian Samson <2115822@csl-student.wlv.ac.uk>
2022-05-06 18:17:02 +0100

    update

--- app/Controllers/FashionC.php
+++ app/Controllers/FashionC.php
@@ -18,5 +18,48 @@ class FashionC extends BaseController
 
     }
 	
+	//saves all data from react
+		public function saveFashionData()
+    {					
+	    $model = model(FashionModel::class);
+		 
+			$result = $model->save([
+            'product_name' => $this->request->getPost('product_name'),      
+            'price'  => $this->request->getPost('price'),
+		    'image'  => $this->request->getPost('image'),
+		     'description'  => $this->request->getPost('description'),
+		    'contact'  => $this->request->getPost('contact'),
+		   'available'  =>1,
+			
+        ]);
+		 
+	echo json_encode($result);	
+			
+	}
+
+	
+	//delete data from react
+	public function deleteFashionData($id){
+		  $model = model(FashionModel::class);
+		$response=$model->delete($id);
+		
+		echo json_encode($response);
+		
+	}
+	
+	
+	//this function will make user not to buy again
+		public function buyme($id){
+			
+	
+			 $model = model(FashionModel::class);
+			 $response=$model->set('available', '0')
+                  ->where('id', $id)
+                  ->update();
+			 
+		echo  json_encode($response);
+		
+	}
+	
 	
 }
--- react/api.js
+++ react/api.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-	
+	  //image  https://www.pexels.com/search/clothes/
 	
 class App extends React.Component {
 
@@ -10,19 +10,116 @@ class App extends React.Component {
 	
 	  constructor(props) {
     super(props);
+	   this.state = { 
+	   fashions: [],
+	   product_name:'',
+	   price:'',
+	   image:'',
+	   description:'',
+	   contact:''   
+	   
+	   };
   
-  //image  https://www.pexels.com/search/clothes/
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+	this.handleDelete = this.handleDelete.bind(this);
+	
+		this.handleBuyMe = this.handleBuyMe.bind(this);
+  
+
     // This binding is necessary to make `this` work in the callback
   
   }
+  handleChange(event) {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({ [nam]: val });
+  }
 
-	state = {
-		fashions: []
-	}
+  handleSubmit(event) {
+
+    event.preventDefault();
+
+    const recipeUrl =
+      "'https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/saveFashionData";
+  
+
+    let formData = new FormData();
+    formData.append("product_name", this.state.product_name);
+    formData.append("price", this.state.price);
+	   formData.append("image", this.state.image);
+    formData.append("description", this.state.description);
+	   formData.append("contact", this.state.contact);
+   
+
+    fetch('https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/saveFashionData', {
+      method: "POST",
+      body: formData,
+    }).then(res => res.json())
+		.then((data) => {
+	console.log(data)
+		})
+		.catch(console.log);
+		
+		
+		fetch('https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/index')
+		.then(res => res.json())
+		.then((data) => {
+		  this.setState({ fashions: data.fashion}) 
+		})
+		.catch(console.log)
+
+    
+  }
+  handleBuyMe(e){
+	  let id=e.target.id;		
+	const recipeUrl = 'https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/buyme/'+ id;
+    const requestMetadata = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    };
 	
+	 fetch(recipeUrl, requestMetadata)
+        .then(res => res.json())
+        .then(recipes => {
+            console.log(recipes);
+		
+			
+		  });
+	  
+  }
+  
+    handleDelete(e) {
+
+	let id=e.target.id;		
+	const recipeUrl = 'https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/deleteFashionData/'+ id;
+    const requestMetadata = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    };
+    fetch(recipeUrl, requestMetadata)
+        .then(res => res.json())
+        .then(recipes => {
+            console.log(recipes);
+		
+			
+		  });
+		 
+		 
+		 
+		 fetch('https://mi-linux.wlv.ac.uk/~2115822/fashion_api/public/index.php/FashionC/index')
+		.then(res => res.json())
+		.then((data) => {
+		  this.setState({ fashions: data.fashion}) 
+		})
+		.catch(console.log)
+		 
+    };
 	
-	
-	 
 	
 	
  
@@ -47,37 +144,35 @@ class App extends React.Component {
 		<div>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-3 px-1 bg-light position-fixed" id="sticky-sidebar">
+        <div class="col-3 col-sm-4 col-md-2 px-1 bg-light position-fixed" id="sticky-sidebar">
             <div class="nav flex-column flex-nowrap vh-100 overflow-auto text-black p-2">
 							<h3>Home</h3>
 
-							<form>
+							<form onSubmit={this.handleSubmit}>
 							
 							<div class="form-group">					
-							<input type="text" class="form-control" id="exampleInputEmail1" name="product_name" placeholder="Enter email" required/>		
+							<input type="text" class="form-control" value={this.state.product_name}  name="product_name"  onChange={this.handleChange} placeholder="Enter product name" required/>		
 							</div>
 
 							<div class="form-group">					
-							<input type="text" class="form-control" id="exampleInputEmail1" name="image" placeholder="image" required/>		
+							<input type="text" class="form-control"  value={this.state.image}  name="image" onChange={this.handleChange} placeholder="image" required/>		
 							</div>		
 							
 							<div class="form-group">					
-							<input type="text" class="form-control" id="exampleInputEmail1" name="price" placeholder="Enter price" required/>		
+							<input type="text" class="form-control" value={this.state.price}  name="price"  onChange={this.handleChange} placeholder="Enter price" required/>		
 							</div>
 
 							<div class="form-group">					
-							<input type="text" class="form-control" id="exampleInputEmail1" name="contact" placeholder="Enter contact" required/>		
+							<input type="text" class="form-control"  value={this.state.contact}  name="contact"  onChange={this.handleChange} placeholder="Enter contact" required/>		
 							</div>
 
-							<div class="form-group">					
-							<input type="text" class="form-control" id="exampleInputEmail1" name="available" placeholder="Enter available or not" required/>		
-							</div>
+							
 
 							<div class="form-group">					
-						   <textarea class="form-control" name="description" id="exampleFormControlTextarea1" placeholder="Enter description" rows="3"></textarea>	
+						   <textarea class="form-control" value={this.state.description}  name="description" id="exampleFormControlTextarea1"  onChange={this.handleChange} placeholder="Enter description" rows="3"></textarea>	
 							</div>							
 			
-							<button type="submit" class="btn btn-primary">Add</button>
+							<button type="submit" class="btn btn-primary">Add product</button>
 							</form>	   
 		   
             </div>
@@ -99,11 +194,11 @@ class App extends React.Component {
 						<h4 class="card-title">{item.contact} </h4>
 					<p class="card-text">{item.description} </p>
 					  	        <button type="button" className="btn btn-outline-success" id={item.id} 
-					onClick={this.handleClickDelete}><a href="#">${item.price} --  Buy me  </a></button>
+					onClick={this.handleBuyMe}>${item.price} --  Buy me </button>
 					
 					
 						        <button style={{float:'right'}} type="button" className="btn btn-outline-danger" id={item.id} 
-					onClick={this.handleClickDelete}>Delete</button>
+					onClick={this.handleDelete}>Delete</button>
 					</div>
 					</div>
 			</div>			
